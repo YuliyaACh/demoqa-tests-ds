d7b09c83ced83c6ee7568bbfbf8512e6e99b9ee9
Yuliya Ch <cherev.work@gmail.com>
2024-07-05 01:38:04 +0500

    add TextBoxTests -1 step

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -2,7 +2,10 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -10,33 +13,43 @@ public class TextBoxTests {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 5000; // default 4000
+        Configuration.browserSize = "1920x1080";
+        Configuration.browser = "chrome";
+
     }
 
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
-        $("#firstName").setValue("Alex");
-        $("#lastName").setValue("Ivanov");
-        $("#userEmail").setValue("alex@egorov.com");
-       //$("#gender-radio-1").click(); //выбор пола
+        $("#firstName").setValue("Yuliya");
+        $("#lastName").setValue("Cherevichenko");
+        $("#userEmail").setValue("test@gmail.com");
+        $("#genterWrapper").$(byText("Female")).click(); //пол
         $("#userNumber").setValue("1234567890");
-        $("#dateOfBirthInput").click();
-       // $("#dateOfBirthInput").click(); //выбор даты календаря
-        // $("#subjectsContainer").click(); //субьект
-        // $("#hobbies-checkbox-3").click(); /хобби
-        // $("#uploadPicture").click(); // файл
+        $("#dateOfBirthInput").click(); //календарь
+        $(".react-datepicker__month-select").selectOption("April");
+        $(".react-datepicker__year-select").selectOption("1991");
+        $("div.react-datepicker__month div.react-datepicker__day.react-datepicker__day--015").click();
+
+        $(".subjects-auto-complete__control.css-yk16xz-control").click();
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
+
+        $("#hobbiesWrapper").setValue("Sports").click(); //хобби
+
+        File testFile = new File("src/test/resources/test.pdf");
+        $("#uploadPicture").uploadFile(testFile);
+
+        $("#uploadPicture").click(); // файл
         $("#currentAddress").setValue("Some street 1");
         //$("#state").click();
        // $("#city").click();
 
 
 
-        $("#output #firstName").shouldHave(text("Alex"));
+      //  $("#output #firstName").shouldHave(text("Alex"));
         $("#output #lastName").shouldHave(text("Ivanov"));
         $("#output #userEmail").shouldHave(text("alex@egorov.com"));
         $("#output #userNumber").shouldHave(text("1234567890"));
